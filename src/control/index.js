0b9f7e5b7aa93d28361ffd55d633096521ee224a
Dmitry Korolev <d.a.korolev@tinkoff.ru>
2018-05-10 13:46:53 +0300

    Fix header import

--- src/control/index.js
+++ src/control/index.js
@@ -1,5 +1,5 @@
 import { h, render } from 'preact'
-import { Header } from './components/header'
+import { Header } from './components/Header'
 import { Blocks } from './components/Blocks'
 import { ThemeProvider } from './components/ThemeProvider'
 
